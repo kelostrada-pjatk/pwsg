062dc5aa74f004fa1c8239fe039f4f3a1ccc5a94
kelostrada <kelostrada@gmail.com>
2012-05-16 21:18:10 +0000

    Lab6 przygotowanie, i tak nie dziala

---
+++ WpfPWSG/Lab6 przygotowanie/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Globalization;
+
+namespace Lab6_przygotowanie
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    [ValueConversion(typeof(Orientation), typeof(Boolean))]
+    public class OrientationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+        object parameter, CultureInfo culture)
+        {
+            return ((Boolean)value) ? Orientation.Horizontal : Orientation.Vertical;
+        }
+
+        public object ConvertBack(object value, Type targetType,
+        object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
