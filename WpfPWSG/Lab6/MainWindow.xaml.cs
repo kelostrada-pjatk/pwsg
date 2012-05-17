2a0cf623fedbcb44f9fb036cd98467d609642ee5
kelostrada@gmail.com <kelostrada@gmail.com@8daa8021-136d-2554-084a-d39ba7f1b012>
2012-05-17 09:38:58 +0000

    Lab6 4 punkty

--- WpfPWSG/Lab6/MainWindow.xaml.cs
+++ WpfPWSG/Lab6/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections.ObjectModel;
 
 namespace Lab6
 {
@@ -19,9 +20,55 @@ namespace Lab6
     /// </summary>
     public partial class MainWindow : Window
     {
+        Chocolates ch;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            ch = (Chocolates)FindResource("chocolates");
+            ch.GenerateChocolates((int)slider1.Value, (int)slider2.Value);
+        }
+
+        private void slider1_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (ch!=null)
+                ch.GenerateChocolates((int)slider1.Value, (int)slider2.Value);
+        }
+
+
+    }
+
+    public class Chocolates : ObservableCollection<Rectangle>
+    {
+        public Chocolates()
+            : base()
+        {
+
+        }
+
+        public void GenerateChocolates(int i, int j)
+        {
+            Clear();
+            Rectangle nowy = new Rectangle();
+            nowy.Fill = Brushes.Red;
+            nowy.StrokeThickness = 4;
+            nowy.Stroke = Brushes.Chocolate;
+            Add(nowy);
+
+            for (int k = 0; k < i * j - 1; k++)
+            {
+                nowy = new Rectangle();
+                nowy.Fill = Brushes.SaddleBrown;
+                nowy.StrokeThickness = 4;
+                nowy.Stroke = Brushes.Chocolate;
+                Add(nowy);
+            }
         }
     }
+
+
+
+
+
 }
